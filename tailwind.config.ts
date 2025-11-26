06f2085d54dbb16e716aac98431adccb6fc61102
difadlyaulhaq <difadlyaulhaq2@gmail.com>
2025-11-27 02:21:50 +0700

    feat add folder management for better maintenance

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+theme: {
+  extend: {
+    colors: {
+      brand: {
+        black: '#000000'; // Kaaba Black
+        gold: '#C5A059';  // Golden Sand
+        white: '#FFFFFF';
+        gray: '#F8F9FA';
+        success: '#28A745';
+      }
+    }
+  }
+}
